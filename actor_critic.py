301f0b2ce1e578c59ce081d40678fbd48ed195a3
Ilya Borovik <ilyaborovik97@gmail.com>
2022-10-22 23:25:48 +0300

    actor-critic: run multiple episodes as batches

--- actor_critic.py
+++ actor_critic.py
@@ -1,5 +1,5 @@
 from collections import defaultdict
-from typing import Optional, Union, Type, Dict
+from typing import Optional, Union, Type, Dict, Callable
 
 import torch
 
@@ -101,55 +101,77 @@ class ActorCriticTrainer:
         else:
             critic_optimizer_params = critic_optimizer_params or {}
             self.critic_optimizer = critic_optimizer(self.critic.parameters(), **critic_optimizer_params)
+            
+    def run_episode(
+        self,
+        init_state: torch.Tensor,
+    ):
+        dynamics, actor = self.dynamics, self.actor
+        episode_actor_loss, episode_critic_loss, episode_reward = 0., 0., 0.
+        
+        t = 0
+        state = init_state
+        while t <= dynamics.total_time:
+            action = actor(dynamics.system.get_observation(state))
+            next_state = dynamics.make_transition(state, action)
+
+            episode_actor_loss += dynamics.actor_loss(state, action=action, next_state=next_state).mean()
+            episode_critic_loss += dynamics.critic_loss(state, action=action, next_state=next_state).mean()
+            episode_reward += dynamics.get_reward(state, action).mean().item()
+
+            state = next_state.detach()
+            t += dynamics.sampling_time
+
+        # steps = dynamics.total_time / dynamics.sampling_time
+        # episode_actor_loss /= steps
+        # episode_critic_loss /= steps
+            
+        return episode_actor_loss, episode_critic_loss, episode_reward
 
     def run(
             self,
             iterations: int = 10,
             episodes: int = 1,
+            episode_as_batch: bool = False,    
+            init_state_sampler: Optional[Callable] = None,
             grad_clip_norm: Optional[float] = None,
             verbose: bool = False
     ):
-        dynamics = self.dynamics
-        actor, critic = dynamics.actor, dynamics.critic
+        dynamics, actor, critic = self.dynamics, self.actor, self.critic
 
         actor_optimizer, critic_optimizer = self.actor_optimizer, self.critic_optimizer
         assert actor_optimizer is not None and critic_optimizer is not None, \
             'Actor/Critic optimizers are not initialized, run `init_optimizers`'
         actor_grad_norm = critic_grad_norm = None
 
+        init_state_fn = dynamics.system.init_state if init_state_sampler is None else init_state_sampler
+        
         training_history = defaultdict(list)
 
         for iteration in range(self.total_iterations, self.total_iterations + iterations):
             actor_losses, critic_losses, rewards = [], [], []
-            for episode in range(episodes):
-                episode_actor_loss, episode_critic_loss, episode_reward = 0., 0., 0.
+            
+            if episode_as_batch:
+                init_state = torch.stack([init_state_fn() for _ in range(episodes)])
+                mean_actor_loss, mean_critic_loss, mean_reward = self.run_episode(init_state=init_state)
+                
+                actor_losses.append(mean_actor_loss)
+                critic_losses.append(mean_critic_loss)
+                rewards.append(mean_reward)
+            else:
+                for episode in range(episodes):
+                    init_state = init_state_fn()
+                    episode_actor_loss, episode_critic_loss, episode_reward = self.run_episode(init_state=init_state)
 
-                t = 0
-                state = dynamics.system.init_state()
-                while t <= dynamics.total_time:
-                    action = actor(dynamics.system.get_observation(state))
-                    next_state = dynamics.make_transition(state, action)
+                    if episodes > 1 and verbose:
+                        print(f'Episode {episode + 1:3d}/{episodes}: '
+                              f'actor_loss={episode_actor_loss.item():.4f}, '
+                              f'critic_loss={episode_critic_loss.item():.4f}, '
+                              f'reward={episode_reward:.4f}')
 
-                    episode_actor_loss += dynamics.actor_loss(state, action=action, next_state=next_state)
-                    episode_critic_loss += dynamics.critic_loss(state, action=action, next_state=next_state)
-                    episode_reward += dynamics.get_reward(state, action).item()
-
-                    state = next_state.detach()
-                    t += dynamics.sampling_time
-
-                steps = dynamics.total_time / dynamics.sampling_time
-                episode_actor_loss /= steps
-                episode_critic_loss /= steps
-
-                if episodes > 1 and verbose:
-                    print(f'Episode {episode + 1:3d}/{episodes}: '
-                          f'actor_loss={episode_actor_loss.item():.4f}, '
-                          f'critic_loss={episode_critic_loss.item():.4f}, '
-                          f'reward={episode_reward:.4f}')
-
-                actor_losses.append(episode_actor_loss)
-                critic_losses.append(episode_critic_loss)
-                rewards.append(episode_reward)
+                    actor_losses.append(episode_actor_loss)
+                    critic_losses.append(episode_critic_loss)
+                    rewards.append(episode_reward)
 
             actor_loss = torch.stack(actor_losses).mean()
             critic_loss = torch.stack(critic_losses).mean()
@@ -174,7 +196,7 @@ class ActorCriticTrainer:
                       f'reward={reward:.4f}'
                       + (f', actor_grad_norm={actor_grad_norm:.4f}' if actor_grad_norm is not None else '')
                       + (f', critic_grad_norm={critic_grad_norm:.4f}' if critic_grad_norm is not None else ''))
-                if episodes > 1:
+                if episodes > 1 and not episode_as_batch:
                     print()
 
             training_history['actor_loss'].append(actor_loss.item())
